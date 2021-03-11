9f92ed068a6e34dfc8f14c05bed17c7bb761c747
dboicourt <dboicourt@gmail.com>
2021-03-10 16:36:04 -0800

    Resource purchase route completed

--- VerseAPI/Controllers/MarketController.cs
+++ VerseAPI/Controllers/MarketController.cs
@@ -20,9 +20,9 @@ namespace VerseAPI.Controllers
         }
 
         [HttpPut]
-        public async Task<IActionResult> PurchaseResources(PurchasePayload payload)
+        public async Task<ActionResult<Ship>> PurchaseResources(PurchasePayload payload)
         {
-            var planet = await _context.Planet.FindAsync(payload.PlanetID);
+            Planet planet = await _context.Planet.FindAsync(payload.PlanetID);
 
             if (planet == null)
             {
@@ -36,11 +36,45 @@ namespace VerseAPI.Controllers
                 return NotFound();
             }
 
-            //transaction
-            var res = payload.Resource;
-            //fix below
-            var planetRes = planet.GetType().GetProperty(res).GetValue(planet, null);
-            var cost = payload.Quantity;
+            //transaction checks
+
+            //get planet resource info
+            var planetResAmount = planet.GetType().GetProperty($"{payload.Resource}Amount").GetValue(planet, null);
+            long resAmount = (long)planetResAmount;
+            long resourceTotal = resAmount - payload.Quantity;
+
+            //check available amount vs payload
+            if (resourceTotal < 0)
+            {
+                return BadRequest($"{planet.Name} does not have sufficient {payload.Resource} for transaction");
+            }
+
+            //find out total cost and check against ship balance
+            var planetResRate = planet.GetType().GetProperty($"{payload.Resource}Price").GetValue(planet, null);
+            long resRate = (long)planetResRate;
+            var totalCost = payload.Quantity * resRate;
+            if (totalCost > ship.Credits)
+            {
+                return BadRequest($"{ship.PilotName} does not have sufficient credits for transaction");
+            }
+
+            //check available capacity of ship
+            if (ship.Capacity < payload.Quantity)
+            {
+                return BadRequest($"{ship.ShipName} does not have enough remaining cargo capacity for transaction");
+            }
+
+            //enact transaction
+
+            //remove resource from planet
+            SetValue(planet, $"{payload.Resource}Amount", resourceTotal);
+
+            //add resource to ship, lower remaining ship capacity, remove credits to pay for transaction
+            long shipResourceTotal = SnagValue(ship, payload.Resource) + payload.Quantity;
+            SetValue(ship, $"{payload.Resource}", shipResourceTotal);
+            ship.Capacity = ship.Capacity - payload.Quantity;
+            ship.Credits = ship.Credits - totalCost;
+
 
             _context.Entry(planet).State = EntityState.Modified;
             _context.Entry(ship).State = EntityState.Modified;
@@ -54,8 +88,18 @@ namespace VerseAPI.Controllers
                 throw;
             }
 
-            //placeholder
-            return NoContent();
+            return ship;
+        }
+
+        public static void SetValue(object obj, string propertyName, object value)
+        {
+            obj.GetType().GetProperty(propertyName).SetValue(obj, value, null);
+        }
+
+        long SnagValue(object obj, string propertyName)
+        {
+            var result = obj.GetType().GetProperty(propertyName).GetValue(obj);
+            return (long)result;
         }
     }
 }
